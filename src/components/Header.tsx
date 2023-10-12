504040c9fdf0ebd4bf5d05e70ca54e153c813167
Elizaveta Kritskaya <post.kri@gmail.com>
2023-10-12 20:33:06 +0300

    add styled components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../styledComponents/Header.css'
 
 export const Header = () => {
     return (
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
+import  s from   '../styledComponents/Profile.module.css'
 
 export const Profile = () => {
     return (
 
-            <div className={'content'}>
-                <div><img src={'https://gas-kvas.com/uploads/posts/2023-02/1675485246_gas-kvas-com-p-fonovie-risunki-na-rabochii-stol-visokogo-22.jpg'}/></div>
+            <div className={s.content}>
+                <div><img className={s.main} src={'https://gas-kvas.com/uploads/posts/2023-02/1675485246_gas-kvas-com-p-fonovie-risunki-na-rabochii-stol-visokogo-22.jpg'}/></div>
 
-                <div><img src={'https://kartinkin.net/uploads/posts/2022-02/1645500423_11-kartinkin-net-p-kvadratnie-kartinki-11.jpg'}/></div>
+                <div><img className={s.profile} src={'https://kartinkin.net/uploads/posts/2022-02/1645500423_11-kartinkin-net-p-kvadratnie-kartinki-11.jpg'}/></div>
                 <div>
                     <div>My post</div>
                     <div>New post</div>
                 </div>
-                <div>
-                    <div>post 1</div>
-                    <div>post 2</div>
-                    <div>post 3</div>
+                <div className={s.posts}>
+                    <div className={s.post}>post 1</div>
+                    <div className={s.post}>post 2</div>
+                    <div className={s.post}>post 3</div>
                 </div>
 
             </div>
